b22128d79c988e438b6ef46cb60191a5f90b28b1
philcz16 <phil16terpasetheo@gmail.com>
2020-07-06 11:07:22 +0000

    Day 2

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -6,31 +6,29 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-        Console.WriteLine("###########################################");
-        Console.WriteLine("Welcome to Result Processing System!!!");
-        Console.WriteLine("By Philcz");
-        Console.WriteLine("###########################################");
-        Console.WriteLine("----------------------------------------------------------------------------------------");
-        Console.WriteLine("Enter your name!");
-         var name = Console.ReadLine();
-        Console.WriteLine($"Welcome {name} ");
-        Console.WriteLine("----------------------------------------------------------------------------------------");
-        Console.WriteLine("Enter number of grades");
-        var grade_num = Convert.ToInt32(Console.ReadLine());
-         Console.WriteLine("Enter grades");
-          List<double> grades = new List<double>();
-          for (int i=1; i<=grade_num;i++){
-              Console.WriteLine($"Enter score {i}:");
-              var score = Convert.ToDouble(Console.ReadLine());
-              grades.Add(score);
-
-        double result = 0.0;
-          foreach(var number in grades){
-             result += number;
-          }
-          result /= grades.Count;
-         Console.WriteLine($"The average score is {result}");
+    Console.WriteLine("##################################################");
+    Console.WriteLine("Result Processing System");
+     Console.WriteLine("By Philcz");
+    Console.WriteLine("##################################################");
+     Console.WriteLine("Enter your name!");
+      Console.WriteLine("-------------------------------------------------------");
+     var name = Console.ReadLine();
+      Console.WriteLine($"Welcome {name}!");
+       Console.WriteLine("-------------------------------------------------------");
+       Console.WriteLine("Enter the number of scores");
+       var number_score = Convert.ToDouble(Console.ReadLine());
+List<double> grades = new List<double>();
+var result = 0.0;
+    for (int i=1; i<=number_score;i++){
+        Console.WriteLine($"Enter score {i}");
+        var score = Convert.ToDouble(Console.ReadLine());
+        grades.Add(score);
+    }
+    foreach (var grade in grades){
+        result +=grade;
+    }
+      result /= grades.Capacity;
+      Console.WriteLine($"The average of your score is {result}");
         }
     }
 }
- 
