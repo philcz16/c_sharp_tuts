e74600af3ec7cd20e9b47c1be8eba231e84f4282
philcz16 <phil16terpasetheo@gmail.com>
2020-07-09 15:42:23 +0000

    Day 3

---
+++ src/GradeBook/Book.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+namespace GradeBook{
+ class Book{
+
+public void AddGrade(double grade)
+{
+
+
+}
+List<double> grades; 
+    }
+}
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -2,14 +2,16 @@
 using System.Collections.Generic;
 namespace GradeBook
 {
+    
     class Program
     {
         static void Main(string[] args)
         {
+            var book = new Book();
             var grades = new List<double> () {12.7,10.3,6.11,4.1};
             grades.Add(56.1);
-            
-            var result = 0.0;
+            book.AddGrade(89.1);
+            var result = 0.0; 
 
             foreach(var number in grades){
                 result +=number;
