bf1964e0cf2e0ed24954a4d634d80668cb3c09de
philcz16 <phil16terpasetheo@gmail.com>
2020-07-10 09:56:01 +0000

    Day 4

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -1,14 +1,17 @@
 using System;
 using System.Collections.Generic;
 
-namespace GradeBook{
+namespace GradeBook{ 
  class Book{
 
+public Book(){
+    grades = new List<double>();
+}
 public void AddGrade(double grade)
 {
 
-
+grades.Add(grade);
 }
 List<double> grades; 
     }
-}
+} 
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -6,19 +6,29 @@ namespace GradeBook
     class Program
     {
         static void Main(string[] args)
-        {
+        { 
+
             var book = new Book();
+            book.AddGrade(89.1);
+            book.AddGrade(90.5);
+            book.AddGrade(77.5);
+             
             var grades = new List<double> () {12.7,10.3,6.11,4.1};
             grades.Add(56.1);
-            book.AddGrade(89.1);
+            
             var result = 0.0; 
-
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
             foreach(var number in grades){
+                highGrade = Math.Max(number,highGrade);
+                lowGrade = Math.Min(number, lowGrade);
                 result +=number;
             }      
             result/=grades.Count;
 
             Console.WriteLine($"The average grade is {result:N1}");  
+            Console.WriteLine($"The Highest grade is {highGrade:N1}"); 
+            Console.WriteLine($"The lowest grade is {lowGrade:N1}");
         }
     }
 }
